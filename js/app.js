9327bd5695391d0dc9e725a6fc1a36a44227c7cf
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-11 23:17:28 -0500

    salessum

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 var california = {
   minCust: 15,
   maxCust: 40,
-  
-
+  salesHourly: [3,6,8,4,10,12,10,15,13,15,20,10,13,7,5],
+  saleTotal: 151,
+  saleAvg: 10.06,
 }
+
+function getsum(array){
+  return array.reduce(function(sum, value) {
+    return sum + value;
+  }, 0);
+}
+
