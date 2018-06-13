06a69d0b40a10dc11342e5a30de6cba87da13813
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-13 14:40:39 -0500

    prototype functions added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,18 @@ function Store (minCust, maxCust, name, saleAvg) {
   this.maxCust = maxCust;
   this.name = name;
   this.saleAvg = saleAvg;
+  this.salesHourly = [];
 }
+
+Store.prototype.randomNum = function(){
+  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+};
+
+Store.prototype.purchaseHour = function(){
+  for(var i = 0; i < 15; i++) {
+    this.salesHourly[i] = Math.floor(this.randomNum() * this.saleAvg);
+  }
+};
 
 var california = new Store (15, 40, 'california', 10.06);
 var colorado = new Store (11, 37, 'colorado', 3.4);
