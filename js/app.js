283eba46d10b928a06fb9e427cd16442cfe2957f
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-12 23:49:57 -0500

    total added

--- js/app.js
+++ js/app.js
@@ -92,6 +92,7 @@ var ohio = {
 
 var hours = ['6am', '7am' , '8am' , '9am' , '10am' , '11am' , '12pm' , '1pm' , '2pm' , '3pm' , '4pm' , '5pm' , '6pm' , '7pm' , '8pm' ];
 california.purchaseHour();
+
 var caliElem = document.getElementById ('california');
 for (var i = 0; i<15; i++) {
   var liElem = document.createElement ('li');
@@ -99,6 +100,14 @@ for (var i = 0; i<15; i++) {
   liElem.textContent = fullList;
   caliElem.appendChild(liElem);
 }
+
+var caltotal =california.salesHourly.reduce(function(acc, val) { return acc + val; });
+var totalElem = document.getElementById ('california');
+var cElem = document.createElement('li');
+cElem.textContent = 'TOTAL: ' + caltotal;
+totalElem.appendChild(cElem);
+
+
 
 colorado.purchaseHour();
 var colElem = document.getElementById ('colorado');
