a2fb80973b3a599499bf0352fcd6eac69a2a72da
AaronPeterson27 <apeterson2727@gmail.com>
2018-06-14 14:49:37 -0500

    Totals Fixed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var hours = ['6am', '7am' , '8am' , '9am' , '10am' , '11am' , '12pm' , '1pm' , '2pm' , '3pm' , '4pm' , '5pm' , '6pm' , '7pm' , '8pm' ];
 
 function Store (minCust, maxCust, name, saleAvg) {
   this.minCust = minCust;
@@ -14,14 +14,8 @@ Store.prototype.randomNum = function(){
 };
 
 Store.prototype.purchaseHour = function(){
-  for(var i = 0; i < 15; i++) {
+  for(var i = 0; i < hours.length; i++) {
     this.salesHourly[i] = Math.floor(this.randomNum() * this.saleAvg);
-
-
-
-
-
-
   }
 };
 
@@ -38,7 +32,10 @@ Store.prototype.render = function() {
     td.textContent = this.salesHourly[i];
     tr.appendChild (td);
   }
-  var total = this.salesHourly.reduce(function(acc, val) { return acc + val; });
+  var total = this.salesHourly.reduce(function(acc, val) {
+    return acc + val;
+  });
+  td = document.createElement ('td');
   td.textContent = total;
   tr.appendChild (td);
 };
@@ -62,33 +59,3 @@ var nebraska = new Store (16, 38, 'Nebraska', 6.2);
 nebraska.render();
 var ohio = new Store (8, 23, 'Ohio', 5.06);
 ohio.render();
-var hours = ['6am', '7am' , '8am' , '9am' , '10am' , '11am' , '12pm' , '1pm' , '2pm' , '3pm' , '4pm' , '5pm' , '6pm' , '7pm' , '8pm' ];
-
-
-// california.purchaseHour();
-
-// var caliElem = document.getElementById ('california');
-// for (var i = 0; i<15; i++) {
-//   var liElem = document.createElement ('li');
-//   var fullList = hours[i] + ': ' +california.salesHourly[i] + ' Cookies.';
-//   liElem.textContent = fullList;
-//   caliElem.appendChild(liElem);
-// }
-
-// var caltotal =california.salesHourly.reduce(function(acc, val) { return acc + val; });
-// var totalElem = document.getElementById ('california');
-// var cElem = document.createElement('li');
-// cElem.textContent = 'TOTAL: ' + caltotal;
-// totalElem.appendChild(cElem);
-
-
-
-// colorado.purchaseHour();
-// var colElem = document.getElementById ('colorado');
-// for (var c = 0; c<15; c++) {
-//   var liCol = document.createElement ('li');
-//   var colList = hours[c] + ': ' + colorado.salesHourly[c] + ' Cookies';
-//   liCol.textContent = colList;
-//   colElem.appendChild(liCol);
-// }
-
